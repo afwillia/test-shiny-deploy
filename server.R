8b424a53ad50986c2b1060c7e3ee07d9a324aae5
afwillia <anthony.williams@sagebase.org>
2023-02-02 13:02:57 -0800

    install poetry

--- server.R
+++ server.R
@@ -5,7 +5,9 @@ server <- function(input, output){
   py_ver <- "3.10.7"
   install_python(version = py_ver)
   use_virtualenv("venv", python_version = py_ver)
-  py_install("schematicpy")
+  message(list.files("/home/shiny/.pyenv/versions/3.10.7"))
+  system("curl -sSL https://install.python-poetry.org | /home/shiny/.pyenv/versions/3.10.7/bin/python3 -")
+  #py_install("schematicpy")
 
   observeEvent(input$action, {
     cat("action button") 
