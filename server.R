e3a91a0ad2bde24032c04caf27dfdeb43b1f1ed4
afwillia <anthony.williams@sagebase.org>
2023-02-02 13:50:09 -0800

    use virtualenv_create()

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ server <- function(input, output){
   
   py_ver <- "3.10.7"
   install_python(version = py_ver)
-  use_virtualenv("venv", python_version = py_ver)
+  virtualenv_create("venv", python_version = py_ver)
   message(list.files("/home/shiny/.pyenv/versions/3.10.7"))
   system("curl -sSL https://install.python-poetry.org | /home/shiny/.pyenv/versions/3.10.7/bin/python3 -")
   #py_install("schematicpy")
