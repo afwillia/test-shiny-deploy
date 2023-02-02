d498c954a3e57f137ea2cff3ad86c2736540ad38
afwillia <anthony.williams@sagebase.org>
2023-02-02 12:43:13 -0800

    install schematicpy

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ server <- function(input, output){
   py_ver <- "3.10.7"
   install_python(version = py_ver)
   use_virtualenv("venv", python_version = py_ver)
+  py_install("schematicpy")
 
   observeEvent(input$action, {
     cat("action button") 
